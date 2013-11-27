5de8b16d2b5fb9fc58b723aa2c7123ee4aaa730d
Karol Herbst <git@karolherbst.de>
2013-11-27 15:23:02 +0100

    cmake/compiler: add an abstract compiler flags for gcc like compilers

---
+++ cmake/Compiler/GccCompatible.cmake
@@ -0,0 +1,10 @@
+# we compile as a shared library and with hidden symbols by default
+add_compiler_flags(-fPIC -fvisibility=hidden)
+
+# we use the new c++11 standard for C++
+# we will use the std=c++0x flag until we run into any incompatibilty.
+# std=c++11 requires gcc-4.7+
+add_compiler_flags(CXX -std=c++0x)
+
+# having as-needed enabled is good practise
+add_linker_flags(-Wl,--as-needed)
