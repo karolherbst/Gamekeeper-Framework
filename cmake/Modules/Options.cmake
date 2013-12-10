8c07b89577a0a80eac3e1ecd6b67c844ed296a1a
Karol Herbst <git@karolherbst.de>
2013-12-10 13:02:40 +0100

    cmake/options: add WITH_FRONTENDS
    so we can decide which frontends to compile

--- cmake/Modules/Options.cmake
+++ cmake/Modules/Options.cmake
@@ -4,3 +4,9 @@
 
 option(BUILD_TESTS "enables the build of unit tests" OFF)
 option(BUILD_PROTOTYPES "enables the build of prototypes" OFF)
+
+#######################################################################################################################
+# module options
+#######################################################################################################################
+
+option(WITH_FRONTENDS "list of frontends to compile" "CXX")
