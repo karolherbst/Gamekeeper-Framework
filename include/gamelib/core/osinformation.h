0ed393be709d794b20a406738a9284e304e64f5a
Karol Herbst <git@karolherbst.de>
2014-01-13 22:34:14 +0100

    core/osinformation: added with Linux stub implementation

---
+++ include/gamelib/core/osinformation.h
@@ -0,0 +1,58 @@
+/*
+ * libgamelib
+ *
+ * Copyright (C) 2013 Karol Herbst <gamelib@karolherbst.de>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+
+#ifndef GAMELIB_CORE_OSINFORMATION_H
+#define GAMELIB_CORE_OSINFORMATION_H 1
+
+#include <gamelib/core/common.h>
+
+#include <gamelib/core/interface.h>
+
+#include <string>
+
+GAMELIB_NAMESPACE_START(core)
+
+/**
+ * @interface OSInformation osinformation.h <gamelib/core/osinformation.h>
+ *
+ * this interface provides all methods to get OS specific paths like where should config files be stored
+ *
+ * @author Karol Herbst
+ * @since 0
+ */
+interface PUBLIC_API OSInformation
+{
+	PUBLIC_API GAMELIB_INTERFACE_DESTRUCTOR(OSInformation)
+
+	/**
+	 * returns the file with the given name from the config paths
+	 *
+	 * @author Karol Herbst
+	 * @since 0
+	 *
+	 * @param name the file name
+	 * @return the resolved file
+	 */
+	PUBLIC_API GAMELIB_INTERFACE_METHOD(std::string getEnv(const char * name));
+};
+
+GAMELIB_NAMESPACE_END(core)
+
+#endif //GAMELIB_CORE_OSINFORMATION_H
