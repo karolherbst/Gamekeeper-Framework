6e7d54307d255a9f08a3b34d78d90c6620f4be92
Karol Herbst <git@karolherbst.de>
2014-01-19 18:54:26 +0100

    test: add first testcase

--- tests/core/linuxinformation.cpp
+++ tests/core/linuxinformation.cpp
@@ -1,5 +1,30 @@
+#include <gamelib/core/common.h>
+#include <gamelib/core/linuxinformation.h>
+
 #include <gtest/gtest.h>
 
-TEST(LinuxInformationTest, Test) {
-    
+using namespace gamelib::core;
+
+class LinuxInformationTest : public testing::Test
+{
+protected:
+	virtual void SetUp() override
+	{
+		this->osInfo = new LinuxInformation();
+	}
+
+	virtual void TearDown() override
+	{
+		delete this->osInfo;
+	}
+
+	OSInformation * osInfo = nullptr;
+};
+
+TEST_F(LinuxInformationTest, testEnvMethodConsistent) {
+	// test that the stored char is not the same
+	std::string home = osInfo->getEnv("HOME");
+	std::string path = osInfo->getEnv("PATH");
+
+	EXPECT_NE(home, path);
 }
