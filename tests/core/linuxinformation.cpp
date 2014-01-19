41dc4282c269e4321b288c0471272c1004e913a4
Karol Herbst <git@karolherbst.de>
2014-01-19 19:03:52 +0100

    tests/core: add some more tests

--- tests/core/linuxinformation.cpp
+++ tests/core/linuxinformation.cpp
@@ -1,9 +1,13 @@
 #include <gamelib/core/common.h>
 #include <gamelib/core/linuxinformation.h>
 
+#include <boost/filesystem/operations.hpp>
+#include <boost/filesystem/path.hpp>
+
 #include <gtest/gtest.h>
 
 using namespace gamelib::core;
+using namespace boost::filesystem;
 
 class LinuxInformationTest : public testing::Test
 {
@@ -21,7 +25,21 @@ protected:
 	OSInformation * osInfo = nullptr;
 };
 
-TEST_F(LinuxInformationTest, getEnvConsistency) {
+TEST_F(LinuxInformationTest, getEnvNotEmpty)
+{
+	std::string home = osInfo->getEnv("HOME");
+	EXPECT_FALSE(home.empty());
+}
+
+TEST_F(LinuxInformationTest, getEnvHomeReturnsFile)
+{
+	path homePath(osInfo->getEnv("HOME"));
+	EXPECT_TRUE(exists(homePath));
+	EXPECT_TRUE(is_directory(homePath));
+}
+
+TEST_F(LinuxInformationTest, getEnvConsistency)
+{
 	// test that the stored char is not the same
 	std::string home = osInfo->getEnv("HOME");
 	std::string path = osInfo->getEnv("PATH");
