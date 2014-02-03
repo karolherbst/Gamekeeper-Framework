eda6ebcf75148b4f12bbb9a1d901fa51c621271a
Karol Herbst <git@karolherbst.de>
2014-02-03 20:18:23 +0100

    cmake/hypodermic: use boost search result from top level cmake

--- cmake/Modules/BuildHypodermic.cmake
+++ cmake/Modules/BuildHypodermic.cmake
@@ -3,7 +3,7 @@ set(HYPODERMIC_INSTALL_DIR ${CMAKE_EXTERNAL_BINARY_DIR}/hypodermic)
 ExternalProject_Add(
   hypodermic
   URL https://github.com/karolherbst/hypodermic/archive/master.zip
-  CMAKE_ARGS -DCMAKE_INSTALL_PREFIX=${HYPODERMIC_INSTALL_DIR} -DBT=SHARED
+  CMAKE_ARGS -DCMAKE_INSTALL_PREFIX=${HYPODERMIC_INSTALL_DIR} -DBT=SHARED -DBOOST_INCLUDEDIR=${Boost_INCLUDE_DIRS} -DBOOST_LIBRARYDIR=${Boost_LIBRARY_DIRS}
   BUILD_IN_SOURCE 0
   SOURCE_DIR <prefix>/src/<name>/hypodermic-master/
 )
