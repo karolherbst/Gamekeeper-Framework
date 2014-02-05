29e38dd41422bb63f222094b3cd3f2d1707a0c09
Karol Herbst <git@karolherbst.de>
2014-02-05 20:25:12 +0100

    core: add windowsinformation class

---
+++ include_private/gamelib/core/windowsinformation.h
@@ -0,0 +1,40 @@
+/*
+ * libgamelib
+ *
+ * Copyright (C) 2013 Karol Herbst <gamelib@karolherbst.de>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+
+#ifndef GAMELIB_CORE_WINDOWSINFORMATION_H
+#define GAMELIB_CORE_WINDOWSINFORMATION_H 1
+
+#include <gamelib/core/common.h>
+
+#include "abstractinformation.h"
+
+GAMELIB_NAMESPACE_START(core)
+
+class PUBLIC_API WindowsInformation : public AbstractInformation
+{
+public:
+	PRIVATE_API GAMELIB_IMPLEMENTATION_OVERRIDE(std::string getEnvSeperator());
+	PRIVATE_API GAMELIB_IMPLEMENTATION_OVERRIDE(boost::filesystem::path getSystemRoot());
+	PRIVATE_API GAMELIB_IMPLEMENTATION_OVERRIDE(boost::filesystem::path getUserPath());
+};
+
+GAMELIB_NAMESPACE_END(core)
+
+#endif //GAMELIB_CORE_WINDOWSINFORMATION_H
