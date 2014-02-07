612e4f7781495b84d3d316e8d585dd4de2b00f46
Karol Herbst <git@karolherbst.de>
2014-02-07 21:14:46 +0100

    cmake/Compiler/GccComp: compile as gnu++0x on MinGW otherwise we can't use GTest

--- cmake/Compiler/GccCompatible.cmake
+++ cmake/Compiler/GccCompatible.cmake
@@ -7,7 +7,11 @@ endif()
 # we use the new c++11 standard for C++
 # we will use the std=c++0x flag until we run into any incompatibilty.
 # std=c++11 requires gcc-4.7+
-add_compiler_flags(CXX -std=c++0x)
+if(MINGW)
+  add_compiler_flags(CXX -std=gnu++0x)
+else()
+  add_compiler_flags(CXX -std=c++0x)
+endif()
 
 # having as-needed enabled is good practise
 add_linker_flags(-Wl,--as-needed)
