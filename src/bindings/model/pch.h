b88b8277ac14f59ccb5dbd1bdc6399de0e12d4ba
Karol Herbst <git@karolherbst.de>
2014-03-03 10:58:35 +0100

    update name in copyright header

--- src/bindings/model/pch.h
+++ src/bindings/model/pch.h
@@ -1,7 +1,7 @@
 /*
- * libgamelib
+ * GameKeeper Framework
  *
- * Copyright (C) 2013 Karol Herbst <gamelib@karolherbst.de>
+ * Copyright (C) 2013 Karol Herbst <gamekeeper@karolherbst.de>
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
