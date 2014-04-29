3482530fcb3d31fed4a881f0f433b1772bf09ebf
Karol Herbst <git@karolherbst.de>
2014-04-29 02:16:36 +0200

    cmake: add warnings we want to fail the build

--- cmake/Compiler/Clang.cmake
+++ cmake/Compiler/Clang.cmake
@@ -1,3 +1,14 @@
 include(${CMAKE_COMPILER_PATH}/GccCompatible.cmake)
 message(STATUS "using clang")
 set(CLANG TRUE)
+
+# disable some clang only warnings
+add_compiler_flags(
+  -Wno-c++98-compat
+  -Wno-c++98-compat-pedantic
+  -Wno-disabled-macro-expansion
+  -Wno-documentation
+  -Wno-exit-time-destructors
+  -Wno-global-constructors
+  -Wno-weak-vtables
+)
