1eea4ebe0faa186042fe3412d7401c75a5268c50
Karol Herbst <git@karolherbst.de>
2014-05-22 14:34:08 +0200

    backend/xmlgamelistparser: parse platforms of games

--- src/backend/xmlgamelistparser.cpp
+++ src/backend/xmlgamelistparser.cpp
@@ -59,12 +59,26 @@ public:
 	pugi::xpath_query gamesListQuery;
 	pugi::xpath_query gameIdQuery;
 	pugi::xpath_query gameNameQuery;
+	pugi::xpath_query platformIdQuery;
+
+	std::string gamePlatformIdsPath;
+
+	std::string platformWin32Id;
+	std::string platformMac32Id;
+	std::string platformLin32Id;
+	std::string platformLin64Id;
 };
 
 XMLGameListParser::PImpl::PImpl(std::map<std::string, std::string> & config)
 :	gamesListQuery(config.at("games.list").c_str()),
 	gameIdQuery(config.at("game.id").c_str()),
-	gameNameQuery(config.at("game.name").c_str()){}
+	gameNameQuery(config.at("game.name").c_str()),
+	platformIdQuery(config.at("platform.id").c_str()),
+	gamePlatformIdsPath(config.at("game.platforms")),
+	platformWin32Id(config.at("platform.win32")),
+	platformMac32Id(config.at("platform.mac32")),
+	platformLin32Id(config.at("platform.lin32")),
+	platformLin64Id(config.at("platform.lin64")){}
 
 XMLGameListParser::XMLGameListParser(std::map<std::string, std::string> & config)
 :	data(new XMLGameListParser::PImpl(config)){}
@@ -82,6 +96,40 @@ XMLGameListParser::parseGameList(std::basic_istream<gkbyte_t> & is)
 			GameXML * game = new GameXML();
 			game->id = this->data->gameIdQuery.evaluate_string(node);
 			game->name = this->data->gameNameQuery.evaluate_string(node);
+
+			// after trivial stuff was parsed, create out variables
+			pugi::xpath_variable_set variables;
+
+			variables.add("game.id", pugi::xpath_type_string);
+			variables.add("gamen.ame", pugi::xpath_type_string);
+
+			pugi::xpath_query gamePlatformIdsQuery(this->data->gamePlatformIdsPath.c_str(), &variables);
+
+			variables.set("game.id", game->getId());
+			variables.set("game.name", game->getName());
+
+			// platform parsing is a little bit more tricky, because we get an array of names or ids
+			for(const pugi::xpath_node & p : gamePlatformIdsQuery.evaluate_node_set(node))
+			{
+				std::string platform = this->data->platformIdQuery.evaluate_string(p);
+				if(platform == this->data->platformWin32Id)
+				{
+					game->platforms.insert(model::Platform::WIN_32);
+				}
+				else if(platform == this->data->platformMac32Id)
+				{
+					game->platforms.insert(model::Platform::MAC_32);
+				}
+				else if(platform == this->data->platformLin32Id)
+				{
+					game->platforms.insert(model::Platform::LIN_32);
+				}
+				else if(platform == this->data->platformLin64Id)
+				{
+					game->platforms.insert(model::Platform::LIN_64);
+				}
+			}
+
 			games.insert(std::move(std::unique_ptr<model::Game>(game)));
 		}
 	}
