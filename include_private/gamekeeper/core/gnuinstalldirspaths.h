1b0cb9838c2446cfdbd4cf0ed29381bf30e40cdf
Karol Herbst <git@karolherbst.de>
2014-05-27 09:56:03 +0000

    core/GNUInstallDirsPaths: added

---
+++ include_private/gamekeeper/core/gnuinstalldirspaths.h
@@ -0,0 +1,37 @@
+/*
+ * GameKeeper Framework
+ *
+ * Copyright (C) 2013 Karol Herbst <gamekeeper@karolherbst.de>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+
+#ifndef GAMEKEEPER_CORE_GNUINSTALLDIRSPATHS_H
+#define GAMEKEEPER_CORE_GNUINSTALLDIRSPATHS_H 1
+
+#include <gamekeeper/core/common.h>
+
+#include <gamekeeper/core/bundlepaths.h>
+
+GAMEKEEPER_NAMESPACE_START(core)
+
+class PUBLIC_API GNUInstallDirsPaths : public BundlePaths
+{
+	PRIVATE_API virtual const boost::filesystem::path & getDataPath() override;
+};
+
+GAMEKEEPER_NAMESPACE_END(core)
+
+#endif //GAMEKEEPER_CORE_GNUINSTALLDIRSPATHS_H
