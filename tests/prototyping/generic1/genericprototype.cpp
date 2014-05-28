b8057ca1c1256a0effe16629ebbe9ede01e5a893
Karol Herbst <git@karolherbst.de>
2014-05-28 12:51:10 +0000

    prototype/generic: remove desura and gameolith and use a generic one

---
+++ tests/prototyping/generic1/genericprototype.cpp
@@ -0,0 +1,86 @@
+#include "genericprototype.h"
+
+#include <boost/program_options/options_description.hpp>
+#include <boost/program_options/variables_map.hpp>
+
+#include <gamekeeper/client/autowire.h>
+#include <gamekeeper/client/storecontroller.h>
+#include <gamekeeper/core/logger.h>
+#include <gamekeeper/core/loggerStream.h>
+
+GAMECLIENTUI_CLASS(GenericPrototype)
+
+namespace client = gamekeeper::client;
+namespace core = gamekeeper::core;
+namespace po = boost::program_options;
+
+GenericPrototype::GenericPrototype(gamekeeper::core::Logger& _logger)
+:	logger(_logger){}
+
+void
+GenericPrototype::init(const ConfigMap & cm)
+{
+}
+
+void
+GenericPrototype::onShutdown()
+{
+}
+
+static std::string
+platformsToString(const std::set<gamekeeper::model::Platform> & platforms)
+{
+	using gamekeeper::model::Platform;
+	std::string result;
+	for(const auto & p : platforms)
+	{
+		switch(p)
+		{
+		case Platform::WIN_32:
+			result += "win32 ";
+			break;
+		case Platform::MAC_32:
+			result += "Mac32 ";
+			break;
+		case Platform::LIN_32:
+			result += "lin32 ";
+			break;
+		case Platform::LIN_64:
+			result += "lin64 ";
+			break;
+		}
+	}
+	return result;
+}
+
+void
+GenericPrototype::startEventLoop()
+{
+	client::Autowire<client::StoreController> sc;
+
+	std::cout << "enter name of game store" << std::endl;
+	std::string storeName;
+
+	while(std::getline(std::cin, storeName))
+	{
+		auto store = sc->get(storeName);
+
+		std::string username;
+		std::string password;
+
+		std::cout << "enter username" << std::endl;
+		std::getline(std::cin, username);
+		std::cout << "enter password" << std::endl;
+		std::getline(std::cin, password);
+
+		store->login(username, password);
+
+		for(const auto & g : store->getAllGames())
+		{
+			this->logger << core::LogLevel::Debug << "id[" << g->getId() << "] name[" << g->getName() << "] platforms[" << platformsToString(g->getPlatforms()) << "]" << core::endl;
+		}
+
+		store->logout();
+		std::cout << "enter name of game store" << std::endl;
+	}
+}
--- tests/prototyping/generic1/genericprototype.h
+++ tests/prototyping/generic1/genericprototype.h
@@ -1,20 +1,17 @@
-#ifndef DESURAPROTOTYPE_H
-#define DESURAPROTOTYPE_H 1
+#ifndef GENERICPROTOTYPE_H
+#define GENERICPROTOTYPE_H 1
 
 #include <gamekeeper/client/gamekeeper.h>
 
-class DesuraPrototype : public gamekeeper::client::GameKeeperUI
+class GenericPrototype : public gamekeeper::client::GameKeeperUI
 {
 public:
-	DesuraPrototype(gamekeeper::core::Logger& logger);
+	GenericPrototype(gamekeeper::core::Logger& logger);
 	virtual void init(const ConfigMap &) override;
 	virtual void onShutdown() override;
 	virtual void startEventLoop() override;
 private:
 	gamekeeper::core::Logger& logger;
-
-	std::string username;
-	std::string userpass;
 };
 
 #endif
