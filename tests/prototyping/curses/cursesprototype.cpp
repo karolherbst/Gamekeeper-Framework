85ecca6166127c31283bd2501291e1da8e5c2ff6
Karol Herbst <git@karolherbst.de>
2014-08-19 12:16:30 +0000

    prototype/curses: add curses stubs

--- tests/prototyping/curses/cursesprototype.cpp
+++ tests/prototyping/curses/cursesprototype.cpp
@@ -1,5 +1,9 @@
 #include "cursesprototype.h"
 
+#include <cwidget/toplevel.h>
+
+namespace cw = cwidget;
+
 GAMECLIENTUI_CLASS(CursesPrototype)
 
 CursesPrototype::CursesPrototype(gamekeeper::core::Logger& _logger)
@@ -8,14 +12,17 @@ CursesPrototype::CursesPrototype(gamekeeper::core::Logger& _logger)
 void
 CursesPrototype::init(const ConfigMap & cm)
 {
+	cw::toplevel::init();
 }
 
 void
 CursesPrototype::onShutdown()
 {
+	cw::toplevel::shutdown();
 }
 
 void
 CursesPrototype::startEventLoop()
 {
+	cw::toplevel::mainloop();
 }
