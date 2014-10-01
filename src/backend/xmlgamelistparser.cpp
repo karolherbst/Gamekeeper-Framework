0509070582f0825a8f811d88e82b6ccb9adb47fd
Karol Herbst <git@karolherbst.de>
2014-10-01 17:06:51 +0200

    WIP backend/gamelistparser: store properties in string maps before creating objects

--- src/backend/xmlgamelistparser.cpp
+++ src/backend/xmlgamelistparser.cpp
@@ -20,9 +20,15 @@
 
 #include <gamekeeper/backend/xmlgamelistparser.h>
 
+#include <boost/algorithm/string/join.hpp>
+
 #include <pugixml.hpp>
 
 #include <gamekeeper/model/genericgame.h>
+#include <gamekeeper/model/converter/gamestringconverter.h>
+#include <gamekeeper/utils/stringutils.h>
+
+namespace balgo = boost::algorithm;
 
 GAMEKEEPER_NAMESPACE_START(backend)
 
@@ -102,58 +108,53 @@ XMLGameListParser::parseGameList(std::basic_istream<gkbyte_t> & is)
 		pugi::xpath_node_set result = this->data->gamesListQuery->evaluate_node_set(doc.document_element());
 		for(const pugi::xpath_node & node : result)
 		{
-			model::GenericGame * game = new model::GenericGame();
-			game->setId(this->data->gameIdQuery->evaluate_string(node));
-			game->setName(this->data->gameNameQuery->evaluate_string(node));
+			std::unordered_map<std::string,std::string> gameProps;
+			gameProps.insert({"game.id", this->data->gameIdQuery->evaluate_string(node)});
+			gameProps.insert({"game.name", this->data->gameNameQuery->evaluate_string(node)});
 			if(this->data->gameDescriptionQuery)
 			{
-				game->setDescription(this->data->gameDescriptionQuery->evaluate_string(node));
+				gameProps.insert({"game.description", this->data->gameDescriptionQuery->evaluate_string(node)});
 			}
 			if(this->data->gameHomepageQuery)
 			{
-				game->setHomepage(this->data->gameHomepageQuery->evaluate_string(node));
+				gameProps.insert({"game.homepage", this->data->gameHomepageQuery->evaluate_string(node)});
 			}
 
 			// after trivial stuff was parsed, create out variables
 			pugi::xpath_variable_set variables;
 
-			variables.add("game.id", pugi::xpath_type_string);
-			variables.add("game.name", pugi::xpath_type_string);
-			variables.add("game.description", pugi::xpath_type_string);
-			variables.add("game.homepage", pugi::xpath_type_string);
+			for(const auto & it : gameProps)
+			{
+				variables.add(it.first.c_str(), pugi::xpath_type_string);
+				variables.set(it.first.c_str(), it.second.c_str());
+			}
 
 			pugi::xpath_query gamePlatformIdsQuery(this->data->gamePlatformIdsPath.c_str(), &variables);
 
-			variables.set("game.id", game->getId().c_str());
-			variables.set("game.name", game->getName().c_str());
-			variables.set("game.description", game->getName().c_str());
-			variables.set("game.homepage", game->getName().c_str());
-
 			// platform parsing is a little bit more tricky, because we get an array of names or ids
-			std::set<model::Platform> platforms;
+			std::set<std::string> platforms;
 			for(const pugi::xpath_node & p : gamePlatformIdsQuery.evaluate_node_set(node))
 			{
 				std::string platform = this->data->platformIdQuery->evaluate_string(p);
 				if(platform == this->data->platformWin32Id)
 				{
-					platforms.insert(model::Platform::WIN_32);
+					platforms.insert(utils::String::toString(model::Platform::WIN_32));
 				}
 				else if(platform == this->data->platformMac32Id)
 				{
-					platforms.insert(model::Platform::MAC_32);
+					platforms.insert(utils::String::toString(model::Platform::MAC_32));
 				}
 				else if(platform == this->data->platformLin32Id)
 				{
-					platforms.insert(model::Platform::LIN_32);
+					platforms.insert(utils::String::toString(model::Platform::LIN_32));
 				}
 				else if(platform == this->data->platformLin64Id)
 				{
-					platforms.insert(model::Platform::LIN_64);
+					platforms.insert(utils::String::toString(model::Platform::LIN_64));
 				}
 			}
-			game->setPlatforms(std::move(platforms));
-
-			games.push_back(std::move(std::unique_ptr<model::Game>(game)));
+			gameProps.insert({"game.platforms", balgo::join(platforms, ",")});
+			games.push_back(model::GameStringConverter::convert(gameProps));
 		}
 	}
 	return games;
